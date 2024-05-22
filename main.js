86b2e2c5b853b437ed8a739d5beb73b414d1ebcd
chauhanyash00 <yashc6128@gmail.com>
2024-05-22 16:35:46 +0530

    add btn

---
+++ main.js
@@ -0,0 +1,13 @@
+function darkbtn(){
+    var a = document.body.style;
+    
+    a.background="black";
+    a.color="black";
+}
+
+function lightbtn(){
+    var a = document.body.style;
+    
+    a.background="white";
+    a.color="black";
+}
